0ee45dc62ed945041078ea38fef663f5f5cb2ce0
Paulo Gabriel Justino Bezerra <pgj1085@gmail.com>
2022-08-10 17:53:52 -0300

    feat/auth: add key column

--- shopping-api/src/main/java/dev/pgjbz/shoppingapi/service/impl/ProductServiceImpl.java
+++ shopping-api/src/main/java/dev/pgjbz/shoppingapi/service/impl/ProductServiceImpl.java
@@ -13,7 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 public class ProductServiceImpl implements ProductService {
 
 
-    private final String BASE_URL = "http://localhost:8081/products/{productIdentifier}";
+    private static final String BASE_URL = "http://localhost:8081/products/{productIdentifier}";
 
     @Override
     public ProductResponseDTO findByIdentifier(String productIdentifier) {
